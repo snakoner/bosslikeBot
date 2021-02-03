4d08afbfd7dbc7fc9188a7139c4109b6a4232aeb
Andrey <stroganov.aa@phystech.edu>
2021-02-03 17:51:46 +0300

    change sh script

--- kill_bosslike_procs.sh
+++ kill_bosslike_procs.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
 var=$(ps aux | grep "bosslike")
-echo $var
+uname=$(id -un)
 arr=(${var// / }])
 arr_len=${#arr[@]}
 for ((i=0;i<$arr_len;i++))
 do
-	if [[ "${arr[$i]}" == "andryusha" ]]; then
+	if [[ "${arr[$i]}" == $uname ]]; then
 		pid=${arr[$i+1]}
-		echo $pid
 		kill -9 $pid
 	fi	
 done
