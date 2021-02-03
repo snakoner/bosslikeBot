0be6f031e34d250f0da2588828fad647f99000e7
Andrey <stroganov.aa@phystech.edu>
2021-02-03 13:15:07 +0300

    added read user data

--- bosslike_insta_likes.py
+++ bosslike_insta_likes.py
@@ -1,20 +1,37 @@
+# Bosslike Bot liker
+# 		this program use selenium module for autoliking at bosslike.ru
+#		@author: 	2021, Andrey Stroganov
+#		@contact:	https://github.com/snakoner/ 
+#		This program is free to use
+#
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 import time
 import copy
 import selenium
 import curses
 
+driver_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+driver_exec_path = '/Users/andryusha/Desktop/chromedriver'
+driver_max_windows_opened = 2
+
 bosslike_url_auth = 'http://bosslike.ru/login/'
 bosslike_username = 'andrstroganov'
 bosslike_password = 'Anacondaz1'
 bosslike_url_likes = 'http://bosslike.ru/tasks/instagram/like/'
+bosslike_refresh_treshhold = 2
 
 insta_url_auth = 'https://www.instagram.com/accounts/login/'
 insta_username = 'ppppk_kkk'
 insta_password = 'Anacondaz1'
 
-
+def read_user_data(filename):
+	data = []
+	with open(filename, 'r') as f:
+		data = f.read().splitlines()
+	return data[0], data[1]
 
 def auth_bosslike(browser, username, password):
 	browser.get(bosslike_url_auth)
@@ -43,20 +60,23 @@ def auth_insta(browser, username, password):
 	pass
 
 if __name__ == '__main__':
+	#driver start
 	opts = Options()
 	opts.headless = True
-	opts.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36")
-	browser = webdriver.Chrome('/Users/andryusha/Desktop/chromedriver', chrome_options=opts)
+	opts.add_argument("user-agent={}".format(driver_user_agent))
+	browser = webdriver.Chrome(driver_exec_path, chrome_options=opts)
 	
+	#auth bosslike + instagram
 	auth_bosslike(browser, bosslike_username, bosslike_password)
 	time.sleep(1)
 	auth_insta(browser, insta_username, insta_password)
 	time.sleep(1)
 	
+	#go to bosslike
 	browser.get(bosslike_url_likes)
+	main_window = browser.current_window_handle
 
-	p = browser.current_window_handle
-
+	#statistics
 	done = 0
 	failed = 0
 
@@ -74,13 +94,14 @@ if __name__ == '__main__':
 
 		time.sleep(2)
 		chwd = browser.window_handles
-		if len(chwd) != 2:
+
+		if len(chwd) != driver_max_windows_opened:
 			failed += 1
 			continue
 		else:
 			time.sleep(6)
 			for w in chwd:
-				if w!=p:
+				if w!=main_window:
 					browser.switch_to.window(w)
 			like_button = browser.find_elements_by_class_name('wpO6b ')
 			if len(like_button):
@@ -88,8 +109,8 @@ if __name__ == '__main__':
 				time.sleep(1)
 				browser.close()
 			time.sleep(1)
-			browser.switch_to.window(p)
-			btns = browser.find_elements_by_class_name("do.btn.btn-sm.btn-primary.btn-block.btn-success.check-task") #change class
+			browser.switch_to.window(main_window)
+			btns = browser.find_elements_by_class_name("do.btn.btn-sm.btn-primary.btn-block.btn-success.check-task")
 			curr_btn = None
 			try:
 				curr_btn = btns.pop(0)
@@ -103,7 +124,7 @@ if __name__ == '__main__':
 			time.sleep(3)
 			done += 1
 			print('Done: {}'.format(done)) 
-		if failed >= 2:
+		if failed > bosslike_refresh_treshhold:
 			browser.get(bosslike_url_likes)
 			print('Refreshing page')
 			time.sleep(3)
